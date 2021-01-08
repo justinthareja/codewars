d20453dd102ea3c1f7716b4364e79fcd4688062c
Justin Thareja <justin.thareja@gmail.com>
2021-01-08 18:25:22 -0500

    updated sum_pairs with naieve solution

--- five_kyu/sum_pairs.js
+++ five_kyu/sum_pairs.js
@@ -19,15 +19,27 @@
 // # * entire pair is earlier, and therefore is the correct answer
 //     == [3, 7]
 
+
+// TODO: MAKE THIS MORE EFFICIENT
 function sumPairs(ints, s) {
-    return [0, 0] || undefined;
+    let result;
+    let x = 0, y = ints.length;
+
+    for (let i = 0; i < ints.length; i ++) {
+        let diff = s - ints[i];
+
+        for (let j = i + 1; j < ints.length; j++) {
+            if (ints[j] === diff && (i >= x ) && ( j <= y )) {
+                x = i;
+                y = j;
+                result = [ints[i], ints[j]];
+            }
+        }
+    }
+
+    return result;
 }
 
-
-console.log(sumPairs([11, 3, 7, 5], 10), [3, 7]);
-console.log(sumPairs([4, 3, 2, 3, 4], 6), [4, 2]);
-console.log(sumPairs([0, 0, -2, 3], 2), undefined);
-console.log(sumPairs([10, 5, 2, 3, 7, 5], 10), [3, 7]);
 
 console.log(sumPairs([1, 4, 8, 7, 3, 15], 8), [1, 7], "Basic: [1, 4, 8, 7, 3, 15] should return [1, 7] for sum = 8");
 console.log(sumPairs([1, -2, 3, 0, -6, 1], -6), [0, -6], "Negatives: [1, -2, 3, 0, -6, 1] should return [0, -6] for sum = -6");
