fc90fcaa384cb80cee95c2ed5a0cba0e5e7f8f7a
Justin Thareja <justin.thareja@gmail.com>
2021-01-12 14:01:52 -0500

    added soltuion to backwards_primes

--- six_kyu/backwards_primes.js
+++ six_kyu/backwards_primes.js
@@ -8,7 +8,8 @@
 
 // Task
 // Find all Backwards Read Primes between two positive given numbers(both inclusive), 
-// the second one always being greater than or equal to the first one.The resulting array or the resulting string will be ordered following the natural order of the prime numbers.
+// the second one always being greater than or equal to the first one.The resulting 
+// array or the resulting string will be ordered following the natural order of the prime numbers.
 
 //     Example
 // backwardsPrime(2, 100) => [13, 17, 31, 37, 71, 73, 79, 97] 
@@ -21,8 +22,43 @@
 // backwardsPrime(2, 100) => [13, 17, 31, 37, 71, 73, 79, 97]
 // backwardsPrime(9900, 10000) => [9923, 9931, 9941, 9967]
 
+
+
 function backwardsPrime(start, stop) {
-    // your code here
+    // uses basic trial division
+    function isPrime(n) {
+        for (let i = 2; i <= Math.ceil(Math.sqrt(n)); i++) {
+            if (n % i === 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+
+    function reverseNumber(n) {
+        n = n.toString();
+        n = n.split("").reverse().join("");
+        return Number(n);
+    }
+
+    function isPalindrome(n) {
+        return n == reverseNumber(n);
+    }
+
+    function isBackwardsPrime(n) {
+        return isPrime(n) && !isPalindrome(n) && isPrime(reverseNumber(n));
+    }
+
+    var result = [];
+
+    for (let i = start; i <= stop; i++) {
+        if (isBackwardsPrime(i)) {
+            result.push(i);
+        }
+    }
+
+    return result;
 }
 
 var a = [9923, 9931, 9941, 9967]
